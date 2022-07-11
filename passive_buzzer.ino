8fa92b7978b9ee88c52f728f9b580ba36c25d0ef
ambukyat <ambukyat@gmail.com>
2022-07-11 16:22:39 +0900

    Update passive_buzzer.ino
    Refactoring
    - melody is a multidimensional array with the format {music note, duration, delay}
    - loop() simplified

--- passive_buzzer.ino
+++ passive_buzzer.ino
@@ -2,63 +2,50 @@
 //2016.12.08
 
 #include "pitches.h"
- 
-// notes in the melody: 
-int melody_line_a[] = {
-  NOTE_D5, NOTE_D5, NOTE_E5, NOTE_D5, NOTE_G5, NOTE_FS5  
-};
 
-int melody_line_b[] = {
-  NOTE_D5, NOTE_D5, NOTE_E5, NOTE_D5, NOTE_A5, NOTE_G5  
-};
+//music note, duration, delay
 
-int melody_line_c[] = {
-  NOTE_D5, NOTE_D5, NOTE_D6, NOTE_B5, NOTE_G5, NOTE_FS5, NOTE_E5
-};
+int melody[][3] = {
+  {NOTE_D5, 500, 500},
+  {NOTE_D5, 500, 500},
+  {NOTE_E5, 500, 500},
+  {NOTE_D5, 500, 500},
+  {NOTE_G5, 500, 500},
+  {NOTE_FS5, 500, 1500},
 
-int melody_line_d[] = {
-  NOTE_C6, NOTE_C6, NOTE_B5, NOTE_G5, NOTE_A5, NOTE_G5
+  {NOTE_D5, 500, 500},
+  {NOTE_D5, 500, 500},
+  {NOTE_E5, 500, 500},
+  {NOTE_D5, 500, 500},
+  {NOTE_A5, 500, 500},
+  {NOTE_G5, 500, 1500},
+
+  {NOTE_D5, 500, 500},
+  {NOTE_D5, 500, 500},
+  {NOTE_D6, 500, 500},
+  {NOTE_B5, 500, 500},
+  {NOTE_G5, 500, 500},
+  {NOTE_FS5, 500, 500},
+  {NOTE_E5, 500, 1500},
+
+  {NOTE_C6, 500, 500},
+  {NOTE_C6, 500, 500},
+  {NOTE_B5, 500, 500},
+  {NOTE_G5, 500, 500},
+  {NOTE_A5, 500, 500},
+  {NOTE_G5, 500, 1500},
 };
-  
-int duration = 500;  // 500 miliseconds
  
 void setup() {
  
 }
  
 void loop() {  
-  for (int thisNote = 0; thisNote < 6; thisNote++) {
-    // pin8 output the voice, every scale is 0.5 sencond
-    tone(8, melody_line_a[thisNote], duration);
-     
-    // Output the voice after several minutes
-    delay(500);
+  for (int thisNote = 0; thisNote < (sizeof(melody) / sizeof(melody[0])); thisNote++) {
+    tone(8, melody[thisNote][0], melody[thisNote][1]);
+
+    delay(melody[thisNote][2]);
   }
-  delay(1000);
-  for (int thisNote = 0; thisNote < 6; thisNote++) {
-    // pin8 output the voice, every scale is 0.5 sencond
-    tone(8, melody_line_b[thisNote], duration);
-     
-    // Output the voice after several minutes
-    delay(500);
-  }
-  delay(1000);
-  for (int thisNote = 0; thisNote < 7; thisNote++) {
-    // pin8 output the voice, every scale is 0.5 sencond
-    tone(8, melody_line_c[thisNote], duration);
-     
-    // Output the voice after several minutes
-    delay(500);
-  }
-  delay(1000);
-   for (int thisNote = 0; thisNote < 6; thisNote++) {
-    // pin8 output the voice, every scale is 0.5 sencond
-    tone(8, melody_line_d[thisNote], duration);
-     
-    // Output the voice after several minutes
-    delay(500);
-  }
-   
-  // restart after two seconds 
-  delay(2000);
+
+  delay(3000);
 }
